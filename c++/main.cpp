3991ddb21a91ee8d74121b1ef1863219a5e9dee0
Michael Castillo <LSx@Michaels-MBP.fios-router.home>
2017-10-21 11:22:40 -0400

    framework for Temp cpp

---
+++ c++/main.cpp
@@ -0,0 +1,33 @@
+/**
+ * Main.cpp
+ *
+ * Homework 1 (Temperature)
+ * Created by Michael Castillo
+ * Date: 10/20/17
+ */
+
+#include <iostream>
+#include <strstream>
+#include "Temperature.h"
+
+Temperature::Temperature(float degrees, int system)
+{
+    this->celsius = degrees;
+    //this->
+}
+
+float Temperature::getCelsius() const
+{
+    return Temperature::CELSIUS;
+}
+
+float Temperature::getFahrenheit() const
+{
+    return Temperature::FAHRENHEIT;
+}
+
+float Temperature::getKelvin() const
+{
+    celsius = ( + 459.67) * 5/9;
+    return Temperature::KELVIN;
+}
