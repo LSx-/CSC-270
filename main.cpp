7ba59eca505753cf1d7bf65e7a0165efc3a360b2
Michael Castillo <LSx@michaels-mbp.adelphi.internal>
2017-10-23 09:11:28 -0400

    homework 1

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/**
+ * main.cpp
+ *
+ * Homework 1 (Temperature)
+ * Created by Michael Castillo
+ * Date: 10/20/17
+ */
+
+#include "Temperature.h"
+#include <iostream>
+
+int main ()
+{
+
+float temp = 68.0;
+
+std::cout << "Fahrenheit: " << temp << std::endl;
+Temperature t (temp,Temperature::FAHRENHEIT);
+
+std::cout << "in Celsius: " << t.getCelsius() << std::endl;
+std::cout << "in Kelvin : " << t.getKelvin() << std::endl;
+
+return 0;
+
+};
