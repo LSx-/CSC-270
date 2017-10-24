7572892b3e95001fbeed2187c10e0c665ceb7c7d
Your Name <you@example.com>
2017-10-24 10:51:30 -0400

    just the very beginning of these files ( comment header and the include statements

---
+++ Person.cpp
@@ -0,0 +1,10 @@
+/**
+* Person.cpp
+*
+* Create by: Michael Castillo
+* Version: 1.0
+* Date: 10/24/17
+*/
+#include <iostream>
+#include "Person.h"
+
---
+++ PersonDriver.cpp
@@ -0,0 +1,18 @@
+/**
+* pDriver.cpp
+*
+* here we will have all the stuff to make it compile
+*
+* Created by: Michael Castillo
+* Version: 1.0
+* Date: 10/24/17
+*/
+
+#include <iostream>
+#include <strstream>
+#include "Person.h"
+
+int main
+{
+
+}
