f7db9dff39915feacfc774e74d50bd500cbbea53
Your Name <you@example.com>
2017-11-20 09:10:04 -0500

    scheme stuff! just started working on them

---
+++ average.rkt
@@ -0,0 +1,9 @@
+#lang scheme
+;;define one or more expressions that can be used to
+;;compute the average of a list
+
+;;insert work below
+
+;;insert work above
+
+(print(average '(3 14 15 9 62 64 37)))
